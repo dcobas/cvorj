12d8bd0070ba71c8371fb0b1637ebe84aaed7e03
Juan David Gonzalez Cobas <dcobas@cern.ch>
2015-01-14 15:49:39 +0100

    add more info fields to ACET script

--- acet.py
+++ acet.py
@@ -6,8 +6,16 @@ import time
 import datetime
 import sys
 
+is_prod = False
+if is_prod:
+    facility = syslog.LOG_LOCAL2
+    environment = 'prod'
+else:
+    facility = syslog.LOG_LOCAL2        # change to something else when available
+    environment = 'dev'
+
 def do_acet_logging(ident, msg, interval):
-    syslog.openlog(ident, 0, syslog.LOG_LOCAL2)
+    syslog.openlog(ident, 0, facility)
     while True:
         timestamp = datetime.datetime.now().strftime('%Y.%m.%d@%H:%M:%S')
         syslog.syslog('%s|time=%s' % (msg, timestamp,))
@@ -19,7 +27,10 @@ if __name__ == '__main__':
     except:
         interval = 20
     ident   = 'driver.encore.ctc'
-    gendata = '%(ident)s gen-by=dcobas|gen-date=11.08.02@09:30:49|encore-version=libencore-7-g19438cc' % locals()
+    gendata = ( 'Environment=dev|MessageType=config|'
+    'SenderType=driver|SenderDomain=encore|SenderSubdomain=ctc|'
+    'SenderFile=ctc.c||'
+    'gen-by=dcobas|gen-date=11.08.02T09:30:49|encore-version=libencore-7-g19438cc|' % locals())
     do_acet_logging(ident, gendata, interval)
         
         
